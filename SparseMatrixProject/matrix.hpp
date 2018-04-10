4700c2ce7089cfe02f1820b84301103cde48c9f4
matrackif <filip.matracki@gmail.com>
2018-04-10 20:30:27 +0200

    Add project files.

---
+++ SparseMatrixProject/matrix.hpp
@@ -0,0 +1,389 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <stdlib.h> 
+#include <cmath>
+/*
+template <class T>
+class matrix2D
+{
+	private:
+		std::vector<T> data;
+		int columnCount;
+		int rowCount;
+
+	public:
+		void print() const;
+		// int getColumnCount(return columnCount;)
+		void print(std::ostream & os) const;
+		T &operator()(int x, int y);
+		friend std::ostream& operator<< (std::ostream& stream, const matrix2D<T>& matrix);
+		friend matrix2D transpose(matrix2D<T>& matrix);
+		friend matrix2D multiply(matrix2D<T> & m1, matrix2D<T> & m2);
+		T* getData(int rowIdx);
+		std::vector<T> getVector();
+		matrix2D(int x, int y);
+};
+template <class T> class matrix2D;
+template <class T>
+std::ostream& operator<<(std::ostream& stream, const matrix2D<T>& matrix);
+template <class T>
+matrix2D<T> transpose(matrix2D<T>& matrix);
+template <class T>
+matrix2D<T> multiply(matrix2D<T> & m1, matrix2D<T> & m2);
+*/
+
+template <class T> class matrix2D;
+
+template <class T>
+std::ostream& operator<<(std::ostream& stream, const matrix2D<T>& matrix);
+
+template <class T>
+matrix2D<T> transpose(matrix2D<T>& matrix);
+
+template <class T>
+bool isValidToMultiply(matrix2D<T> & m1, matrix2D<T> & m2);
+
+template <class T>
+bool isValidToAdd(matrix2D<T> & m1, matrix2D<T> & m2);
+
+template <class T>
+matrix2D<T> multiply(matrix2D<T> & m1, matrix2D<T> & m2);
+
+template <class T>
+matrix2D<T> add(matrix2D<T> & m1, matrix2D<T> & m2);
+
+template <class T>
+class matrix2D
+{
+public:
+	matrix2D();
+	matrix2D(int rowCount, int colCount);
+	matrix2D(int rowCount, int colCount, T fill);
+	void print() const;
+	// int getColumnCount(return columnCount;)
+	void print(std::ostream & os) const;
+	T &operator()(int x, int y);
+	T* getData(int rowIdx);
+	std::vector<T> getVector();
+	int getRowCount();
+	int getColumnCount();
+	int getNumElements();
+	matrix2D<T> createSubMatrix(int startRow, int endRow, int startCol, int endCol);
+	matrix2D<T> & getSubMatrix(int startRow, int endRow, int startCol, int endCol);
+	void copyValues(matrix2D<T> & from, int startRow, int endRow);
+	void copyValues(matrix2D<T> & from, int startRow, int endRow, int startCol, int endCol);
+	friend std::ostream& operator<< <>(std::ostream& stream, const matrix2D<T>& matrix);
+	friend matrix2D<T> transpose <>(matrix2D<T>& matrix);
+	friend bool isValidToMultiply <>(matrix2D<T> & m1, matrix2D<T> & m2);
+	friend bool isValidToAdd <>(matrix2D<T> & m1, matrix2D<T> & m2);
+	friend matrix2D<T> multiply <>(matrix2D<T> & m1, matrix2D<T> & m2);
+	friend matrix2D<T> add <>(matrix2D<T> & m1, matrix2D<T> & m2);
+private:
+	std::vector<T> data;
+	int columnCount;
+	int rowCount;
+	
+};
+template <class T>
+matrix2D<T>::matrix2D() : data(), rowCount(0), columnCount(0)
+{
+
+}
+
+template <class T>
+matrix2D<T>::matrix2D(int x, int y) : data(x*y), rowCount(x), columnCount(y)
+{
+
+}
+
+template <class T>
+matrix2D<T>::matrix2D(int x, int y, T fill) : data(x*y, fill), rowCount(x), columnCount(y)
+{
+
+}
+
+template <class T>
+void matrix2D<T>::print() const
+{
+	for (int i = 0; i < rowCount; i++)
+	{
+		for (int j = 0; j < columnCount; j++)
+		{
+			std::cout << data[i * columnCount + j] << ", ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+template <class T>
+void matrix2D<T>::print(std::ostream & os) const
+{
+	for (int i = 0; i < rowCount; i++)
+	{
+		for (int j = 0; j < columnCount; j++)
+		{
+			os << data[i * columnCount + j] << ", ";
+		}
+		os << std::endl;
+	}
+}
+
+template <class T>
+T& matrix2D<T>::operator()(int x, int y)
+{
+	return data[x * columnCount + y];
+}
+
+template <class T>
+T* matrix2D<T>::getData(int rowIdx)
+{
+	return &data[rowIdx * columnCount];
+}
+
+template <class T>
+std::vector<T> matrix2D<T>::getVector()
+{
+	return data;
+}
+
+template <class T>
+int matrix2D<T>::getRowCount()
+{
+	return rowCount;
+}
+
+template <class T>
+int matrix2D<T>::getColumnCount()
+{
+	return columnCount;
+}
+
+template <class T>
+int matrix2D<T>::getNumElements()
+{
+	return rowCount * columnCount;
+}
+
+template <class T>
+matrix2D<T> matrix2D<T>::createSubMatrix(int startRow, int endRow, int startCol, int endCol)
+{
+	if (startRow < 0 || startRow > rowCount - 1 || startCol < 0 || startCol > columnCount - 1
+		|| endRow < 0 || endRow > rowCount - 1 || endCol < 0 || endCol > columnCount - 1
+		|| startRow > endRow || startCol > endCol)
+	{
+		std::cerr << "createSubMatrix(). Invalid input. startRow=" << startRow << " endRow=" << endRow
+			<< " startCol=" << startCol << " endCol=" << endCol << " this->rowCount=" << rowCount << " this->columnCount=" << columnCount << std::endl;
+		return matrix2D<T>();
+	}
+	matrix2D<T> sub((endRow - startRow) + 1, (endCol - startCol) + 1);
+	int row = 0, col = 0;
+	for (int i = startRow; i <= endRow; i++)
+	{
+		for (int j = startCol; j <= endCol; j++)
+		{
+			sub(row, col) = (*(this))(i, j);
+			col++;
+		}
+		col = 0;
+		row++;
+	}
+	return sub;
+}
+
+template <class T>
+matrix2D<T> & matrix2D<T>::getSubMatrix(int startRow, int endRow, int startCol, int endCol)
+{
+	if (startRow < 0 || startRow > rowCount - 1 || startCol < 0 || startCol > columnCount - 1
+		|| endRow < 0 || endRow > rowCount - 1 || endCol < 0 || endCol > columnCount - 1
+		|| startRow > endRow || startCol > endCol)
+	{
+		std::cerr << "createSubMatrix(). Invalid input. startRow=" << startRow << " endRow=" << endRow
+			<< " startCol=" << startCol << " endCol=" << endCol << " this->rowCount=" << rowCount << " this->columnCount=" << columnCount << std::endl;
+		return *this;
+	}
+	int newRowCount = (endRow - startRow) + 1;
+	int newColumnCount = (endCol - startCol) + 1;
+	int row = 0, col = 0;
+	for (int i = startRow; i <= endRow; i++)
+	{
+		for (int j = startCol; j <= endCol; j++)
+		{
+			(*(this))(row, col) = (*(this))(i, j);
+			col++;
+		}
+		col = 0;
+		row++;
+	}
+	this->data.resize(newRowCount * newColumnCount);
+	return *this;
+}
+
+template <class T>
+void  matrix2D<T>::copyValues(matrix2D<T> & from, int startRow, int endRow)
+{
+	int fromRowCount = from.getRowCount();
+	int fromColCount = from.getColumnCount();
+	int numRowsToFillIn = (endRow - startRow) + 1;
+	if (startRow < 0 || startRow > rowCount - 1 || endRow < 0 || endRow > rowCount - 1 ||
+		fromColCount > columnCount)
+	{
+		std::cerr << "copyValues(). Invalid input. startRow=" << startRow << " endRow=" << endRow
+			<< " this->rowCount=" << rowCount << " this->columnCount=" << columnCount << std::endl;
+		return;
+	}
+	int row = 0, col = 0;
+	for (int i = startRow; i <= endRow; i++)
+	{
+		for (int j = 0; j < fromColCount; j++)
+		{
+			(*(this))(i, j) = from(row, col);
+			col++;
+		}
+		col = 0;
+		row++;
+		// Stop if we want to copy more rows than from matrix actually has
+		if (row >= fromRowCount)
+		{
+			break;
+		}
+	}
+}
+
+template <class T>
+void matrix2D<T>::copyValues(matrix2D<T> & from, int startRow, int endRow, int startCol, int endCol)
+{
+	// int numRowsToFillIn = (endRow - startRow) + 1;
+	// int numColsToFillIn = (endCol - startCol) + 1;
+	int fromRowCount = from.getRowCount();
+	int fromColCount = from.getColumnCount();
+	// if from is larger than "this" matrix then will fill in as many values as possible from top right
+	if (startRow < 0 || startRow > rowCount - 1 || startCol < 0 || startCol > columnCount - 1
+		|| endRow < 0 || endRow > rowCount - 1 || endCol < 0 || endCol > columnCount - 1
+		|| startRow > endRow || startCol > endCol /* || from.getColumnCount() > columnCount
+		|| from.getRowCount() > rowCount */)
+	{
+		std::cerr << "copyValues(). Invalid input. startRow=" << startRow << " endRow=" << endRow
+			<< " startCol=" << startCol << " endCol=" << endCol << " this->rowCount=" << rowCount << " this->columnCount=" << columnCount << std::endl;
+		return;
+	}
+	int row = 0, col = 0;
+	for (int i = startRow; i <= endRow; i++)
+	{
+		for (int j = startCol; j <= endCol; j++)
+		{
+			(*(this))(i, j) = from(row, col);
+			col++;
+			// Stop if we want to copy more cols than from matrix actually has
+			if (col >= fromColCount)
+			{
+				break;
+			}
+		}
+		col = 0;
+		row++;
+		// Stop if we want to copy more rows than from matrix actually has
+		if (row >= fromRowCount)
+		{
+			break;
+		}
+	}
+}
+
+template <class T>
+std::ostream& operator<< (std::ostream& stream, const matrix2D<T>& matrix)
+{
+	matrix.print(stream);
+	return stream;
+}
+
+
+
+template <class T>
+matrix2D<T> transpose(matrix2D<T>& matrix)
+{
+	int n = matrix.columnCount;
+	matrix2D<T> matrixT(n, n);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			matrixT(i, j) = matrix(j, i);
+		}
+	}
+	return matrixT;
+}
+
+template <class T>
+bool isValidToMultiply(matrix2D<T> & m1, matrix2D<T> & m2)
+{
+	bool isValid = true;
+	if (m1.columnCount != m2.rowCount)
+	{
+		std::cerr << "Error in isValidToMultiply(). Num of columns in m1 (" << m1.columnCount << ") is not equal to numbers of rows in m2 ("
+			<< m2.rowCount << ")" << std::endl;
+		isValid = false;
+	}
+	return isValid;
+}
+
+template <class T>
+bool isValidToAdd(matrix2D<T> & m1, matrix2D<T> & m2)
+{
+	bool isValid = true;
+	if (m1.rowCount != m2.rowCount || m1.columnCount != m2.columnCount)
+	{
+		std::cerr << "Error in isValidToAdd(). Dimensions must match. Got dimensions of : (" << m1.rowCount 
+			<< "x" << m1.columnCount << ") and (" << m2.rowCount << "x" << m2.columnCount << ")" << std::endl;
+		isValid = false;
+	}
+	return isValid;
+}
+
+template <class T>
+matrix2D<T> multiply(matrix2D<T> & m1, matrix2D<T> & m2)
+{
+	int nRows = m1.rowCount;
+	int nCols = m2.columnCount;
+	int nColsM1 = m1.columnCount;
+	matrix2D<T> result(nRows, nCols);
+	if (!isValidToMultiply(m1, m2))
+	{
+		return result;
+	}
+	for (int i = 0; i < nRows; i++)
+	{
+		for (int j = 0; j < nCols; j++)
+		{
+			// Dot product of ith row from m1 and jth column from m2
+			for (int k = 0; k < nColsM1; k++)
+			{
+				result(i, j) += m1(i, k) * m2(k, j);
+				// std::cout << "i=" << i << " j=" << j << " k=" << k << std::endl;
+			}
+		}
+	}
+	return result;
+}
+
+template <class T>
+matrix2D<T> add(matrix2D<T> & m1, matrix2D<T> & m2)
+{
+	int nRows = m1.rowCount;
+	int nCols = m1.columnCount;
+	matrix2D<T> result(nRows, nCols);
+	if (!isValidToAdd(m1, m2))
+	{
+		return result;
+	}
+	for (int i = 0; i < nRows; i++)
+	{
+		for (int j = 0; j < nCols; j++)
+		{
+			result(i, j) = m1(i, j) + m2(i, j);
+		}
+	}
+	return result;
+}
+
+
---
+++ SparseMatrixProject/mpi_utils.hpp
@@ -0,0 +1,143 @@
+#pragma once
+#include "matrix.hpp"
+#include "mpi.h"
+template <class T>
+void parallelMult(int rank, int size, matrix2D<T> & m1, matrix2D<T> & m2)
+{
+	const int tag = 0;
+	MPI_Status status;
+	// TODO have rank 0 bcast matrix dims to all other ranks
+	int m1RowCount = m1.getRowCount();
+	int m1ColCount = m1.getColumnCount();
+	int m2RowCount = m2.getRowCount();
+	int m2ColCount = m2.getColumnCount();
+	MPI_Bcast(&m1RowCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&m1ColCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&m2RowCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&m2ColCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	std::cout << "Rank=" << rank << " m1RowCount: " << m1RowCount << " m1ColCount: " << m1ColCount << " m2RowCount: " << m2RowCount << " m2ColCount: " << m2ColCount << std::endl;
+	std::cout << "Rank=" << rank << " matrix m1: \n" << m1 << std::endl;
+	std::cout << "Rank=" << rank << " matrix m2: \n" << m2 << std::endl;
+	if (m1RowCount <= 0 || m1ColCount <= 0 || m2RowCount <= 0 || m2ColCount <= 0)
+	{
+		if (rank == 0)
+		{
+			std::cerr << "parallelMult() Error: One of the matrix dimensions is <= 0" << std::endl;
+		}
+		return;
+	}
+
+
+	matrix2D<T> result, m1SubMatrix, m2SubMatrix;
+	int numRowsPerProcess = m1RowCount / size;
+	if (rank == 0)
+	{
+		int offset = (m1RowCount % size) + (m1RowCount / size);
+		//std::cout << "m1RowCount: " << m1RowCount << " m1ColCount: " << m1ColCount << " m2RowCount: " << m2RowCount << " m2ColCount: " << m2ColCount << std::endl;
+		std::cout << "offset: " << offset << " numRowsPerProcess: " << numRowsPerProcess << std::endl;
+		//std::cout << "matrix m1: \n" << m1 << std::endl;
+		//std::cout << "matrix m2: \n" << m2 << std::endl;
+		result = matrix2D<T>(m1RowCount, m2ColCount);
+		// Send rest of matrix, then calculate
+		for (int d = 0; d < size - 1; d++)
+		{
+			MPI_Send(m1.getData(offset + (d * numRowsPerProcess)), m1ColCount * numRowsPerProcess, MPI_DOUBLE, d + 1, tag, MPI_COMM_WORLD);
+			MPI_Send(m2.getData(0), m2.getNumElements(), MPI_DOUBLE, d + 1, tag, MPI_COMM_WORLD); // TODO partition it differently
+		}
+		m1SubMatrix = m1.createSubMatrix(0, offset - 1, 0, m1ColCount - 1);
+		m2SubMatrix = m2.createSubMatrix(0, m2RowCount - 1, 0, m2ColCount - 1); // TODO partition it differently
+		std::cout << "Rank="<< rank << " m1 submatrix: \n" << m1SubMatrix << std::endl;
+		std::cout << "Rank=" << rank << " m2 submatrix: \n" << m2SubMatrix << std::endl;
+		matrix2D<T> partialResult = multiply(m1SubMatrix, m2SubMatrix);
+		std::cout << "Rank=" << rank << " multiply() m1SubMatrix* m2SubMatrix \n" << partialResult << std::endl;
+		result.copyValues(partialResult, 0, offset - 1);
+		std::cout << "Rank=" << rank << " copyValues() done" << std::endl;
+		for (int i = 0; i < size - 1; i++)
+		{
+			MPI_Recv(result.getData(offset + (i * numRowsPerProcess)), numRowsPerProcess * m2ColCount, MPI_DOUBLE, i + 1, tag, MPI_COMM_WORLD, &status);
+		}
+		std::cout << "parallelMult() Finished! m1: \n" << m1 << "m2: \n" << m2 << " m1 * m2: \n" << result << std::endl;
+	}
+	else
+	{
+		result = matrix2D<T>(numRowsPerProcess, m2ColCount);
+		m1SubMatrix = matrix2D<T>(numRowsPerProcess, m1ColCount);
+		m2SubMatrix = matrix2D<T>(m2RowCount, m2ColCount); // TODO partition it differently
+		MPI_Recv(m1SubMatrix.getData(0), m1SubMatrix.getNumElements(), MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
+		MPI_Recv(m2SubMatrix.getData(0), m2SubMatrix.getNumElements(), MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
+		std::cout << "Rank=" << rank << " m1 submatrix: \n" << m1SubMatrix << std::endl;
+		std::cout << "Rank=" << rank << " m2 submatrix: \n" << m2SubMatrix << std::endl;
+		matrix2D<T> partialResult = multiply(m1SubMatrix, m2SubMatrix);
+		MPI_Send(partialResult.getData(0), partialResult.getNumElements(), MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
+	}
+}
+
+template <class T>
+void parallelAdd(int rank, int size, matrix2D<T> & m1, matrix2D<T> & m2)
+{
+	const int tag = 0;
+	MPI_Status status;
+	// TODO have rank 0 bcast matrix dims to all other ranks
+	int m1RowCount = m1.getRowCount();
+	int m1ColCount = m1.getColumnCount();
+	int m2RowCount = m2.getRowCount();
+	int m2ColCount = m2.getColumnCount();
+	MPI_Bcast(&m1RowCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&m1ColCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&m2RowCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&m2ColCount, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	std::cout << "Rank=" << rank << " m1RowCount: " << m1RowCount << " m1ColCount: " << m1ColCount << " m2RowCount: " << m2RowCount << " m2ColCount: " << m2ColCount << std::endl;
+	std::cout << "Rank=" << rank << " matrix m1: \n" << m1 << std::endl;
+	std::cout << "Rank=" << rank << " matrix m2: \n" << m2 << std::endl;
+	if (m1RowCount != m2RowCount || m1ColCount != m2ColCount)
+	{
+		if (rank == 0)
+		{
+			std::cerr << "parallelAdd() Error: Dimensions msut be equal" << std::endl;
+		}
+		return;
+	}
+
+	matrix2D<T> result, m1SubMatrix, m2SubMatrix;
+	int numRowsPerProcess = m1RowCount / size;
+	if (rank == 0)
+	{
+		int offset = (m1RowCount % size) + (m1RowCount / size);
+		//std::cout << "m1RowCount: " << m1RowCount << " m1ColCount: " << m1ColCount << " m2RowCount: " << m2RowCount << " m2ColCount: " << m2ColCount << std::endl;
+		std::cout << "offset: " << offset << " numRowsPerProcess: " << numRowsPerProcess << std::endl;
+		//std::cout << "matrix m1: \n" << m1 << std::endl;
+		//std::cout << "matrix m2: \n" << m2 << std::endl;
+		result = matrix2D<T>(m1RowCount, m1ColCount);
+		// Send rest of matrix, then calculate
+		for (int d = 0; d < size - 1; d++)
+		{
+			MPI_Send(m1.getData(offset + (d * numRowsPerProcess)), m1ColCount * numRowsPerProcess, MPI_DOUBLE, d + 1, tag, MPI_COMM_WORLD);
+			MPI_Send(m2.getData(offset + (d * numRowsPerProcess)), m2ColCount * numRowsPerProcess, MPI_DOUBLE, d + 1, tag, MPI_COMM_WORLD); // TODO partition it differently
+		}
+		m1SubMatrix = m1.createSubMatrix(0, offset - 1, 0, m1ColCount - 1);
+		m2SubMatrix = m2.createSubMatrix(0, offset - 1, 0, m2ColCount - 1); 
+		std::cout << "Rank=" << rank << " m1 submatrix: \n" << m1SubMatrix << std::endl;
+		std::cout << "Rank=" << rank << " m2 submatrix: \n" << m2SubMatrix << std::endl;
+		matrix2D<T> partialResult = add(m1SubMatrix, m2SubMatrix);
+		std::cout << "Rank=" << rank << " add() m1SubMatrix + m2SubMatrix \n" << partialResult << std::endl;
+		result.copyValues(partialResult, 0, offset - 1);
+		std::cout << "Rank=" << rank << " copyValues() done" << std::endl;
+		for (int i = 0; i < size - 1; i++)
+		{
+			MPI_Recv(result.getData(offset + (i * numRowsPerProcess)), numRowsPerProcess * m2ColCount, MPI_DOUBLE, i + 1, tag, MPI_COMM_WORLD, &status);
+		}
+		std::cout << "parallelAdd() Finished! m1: \n" << m1 << "m2: \n" << m2 << " m1 + m2: \n" << result << std::endl;
+	}
+	else
+	{
+		result = matrix2D<T>(numRowsPerProcess, m1ColCount);
+		m1SubMatrix = matrix2D<T>(numRowsPerProcess, m1ColCount);
+		m2SubMatrix = matrix2D<T>(numRowsPerProcess, m2ColCount); // TODO partition it differently
+		MPI_Recv(m1SubMatrix.getData(0), m1SubMatrix.getNumElements(), MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
+		MPI_Recv(m2SubMatrix.getData(0), m2SubMatrix.getNumElements(), MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
+		std::cout << "Rank=" << rank << " m1 submatrix: \n" << m1SubMatrix << std::endl;
+		std::cout << "Rank=" << rank << " m2 submatrix: \n" << m2SubMatrix << std::endl;
+		matrix2D<T> partialResult = add(m1SubMatrix, m2SubMatrix);
+		MPI_Send(partialResult.getData(0), partialResult.getNumElements(), MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
+	}
+}
